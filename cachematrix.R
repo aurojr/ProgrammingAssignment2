4988ddda30becf9cf351ca97a467c281606c6234
Auro do N. Pimentel Jr <auro.np@gmail.com>
2015-05-24 12:48:27 -0300

    First version, already with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+#' @author Auro
+#' The functions bellow create a group of helping functions, created by the \code{makeCacheMatrix},
+#' which caches an inverse of a given matrix inside it.
+#' Be sure of creating the cache struture, via \code{makeCacheMatrix}, before using the cache itself
 
-## Write a short comment describing this function
+#' This function stores the matrix whose inverse will be cached
+#' @param cache.matrix The matrix that will be cached
+#' @return A list with functions to manipulate the matrix and the inverse cache
+makeCacheMatrix <- function(cache.matrix = matrix()) {
+  solve <- NULL
+  set <- function(matrix) {
 
-makeCacheMatrix <- function(x = matrix()) {
+    # The cache is reset only if the matrix was updated
+    if(!all.equal(cache.matrix, matrix)) {
+      cache.matrix <<- matrix
+      solve <<- NULL
+    }
+  }
 
+  get <- function() cache.matrix
+
+  setSolve <- function(s) solve <<- s
+  getSolve <- function() solve
+
+  list(set = set, get = get, setSolve = setSolve, getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
+#' Retrieve the inverse of the given cached matrix (created by the \code{makeCacheMatrix} function)
+#' @param cached.matrix
+cacheSolve <- function(cached.matrix, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  solve <- cached.matrix$getSolve()
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  if(!is.null(solve)) {
+    message("Getting cached data")
+    return(solve)
+  }
+
+  matrix <- cached.matrix$get()
+
+  solve <- solve(matrix)
+
+  cached.matrix$setSolve(solve)
+
+  solve
 }
